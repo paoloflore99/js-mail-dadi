fe3f12f02a3a41807a2b4bc3eb6fd4a6860cfc2a
paoloflore99 <paoloflore937@gmail.com>
2023-07-06 00:42:19 +0200

    continuo domani

---
+++ Gioco dei dadi/gioco.js
@@ -0,0 +1,13 @@
+//nota bene coomenta con detaglio stavolta tutto che che fai ricorda che e utilissimo 
+
+const generatore = document.querySelector(".generatore")  //cerca la classe non id con il . inizziale 
+
+generatore.addEventListener("click", function () { //serve per quando si fa clik 
+
+    console.log("funziona") //serve per vedere se funziona il tasto 
+    const eta = nome.Value;//prova da rivedere in classe oppure dasolo
+})
+
+
+//generare un numero pari e maggiore di 6
+//stabilire il vincitore in base al risultato 
